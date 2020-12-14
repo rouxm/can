e622b0b272d9148b1ca70d4c7d0242a4d5353616
rouxm <martin_roux@hotmail.com>
2020-12-14 15:34:45 -0500

    3 files modified

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,34 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-//import { ComposeMessageComponent } from './compose-message/compose-message.component';
-//import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
-
-//import { AuthGuard } from './auth/auth.guard';
-//import { SelectivePreloadingStrategyService } from './selective-preloading-strategy.service';
-
 
 
 const appRoutes: Routes = [
-//  {
-//    path: 'compose',
-//    component: ComposeMessageComponent,
-//    outlet: 'popup'
- // },
-  //{
-  //  path: 'admin',
-  //  loadChildren: () => import('./admin/admin.module').then(m => m.AdminModule),
-  //  canLoad: [AuthGuard]
-  //},
-  
+
   {
     path: 'crisis-center',
     loadChildren: () => import('./crisis-center/crisis-center.module').then(m => m.CrisisCenterModule),
     data: { preload: true }
   },
   
- // { path: '',   redirectTo: '/superheroes', pathMatch: 'full' },
- // { path: '**', component: PageNotFoundComponent }
+  { path: '',   redirectTo: '/crisis-center', pathMatch: 'full' },
 ];
 
 @NgModule({
