fb1c618dda1c05fab642b7ca89a9b7c88858c4da
rouxm <martin_roux@hotmail.com>
2020-12-14 15:43:13 -0500

    2 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,38 +6,25 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { Router } from '@angular/router';
 
 import { AppComponent } from './app.component';
-//import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
-//import { ComposeMessageComponent } from './compose-message/compose-message.component';
 
 import { AppRoutingModule } from './app-routing.module';
 
-//import { HeroesModule } from './heroes/heroes.module';
-//import { CrisisCenterModule } from './crisis-center/crisis-center.module';
-
-//import { AuthModule } from './auth/auth.module';
 
 @NgModule({
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     FormsModule,
-  //   AuthModule,
-   // CrisisCenterModule,
     AppRoutingModule,
   ],
   declarations: [
     AppComponent,
-   // ComposeMessageComponent,
- //   PageNotFoundComponent
   ],
   bootstrap: [ AppComponent ]
 })
 export class AppModule {
-  // Diagnostic only: inspect router configuration
-  constructor(router: Router) {
-    // Use a custom replacer to display function names in the route configs
-    // const replacer = (key, value) => (typeof value === 'function') ? value.name : value;
 
-    // console.log('Routes: ', JSON.stringify(router.config, replacer, 2));
+  constructor(router: Router) {
+
   }
 }
