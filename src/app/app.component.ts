bb269a32dcb1afb2f6bc5b51fc233aa820f069d2
rouxm <martin_roux@hotmail.com>
2020-12-14 15:29:43 -0500

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { slideInAnimation } from './animations';
 
 @Component({
   // selector: 'my-app',
@@ -10,8 +10,8 @@ import { RouterOutlet } from '@angular/router';
  // animations: [ slideInAnimation ]
 })
 export class AppComponent {
- // getAnimationData(outlet: RouterOutlet) {
- //   return outlet && outlet.activatedRouteData && outlet.activatedRouteData.animation;
+  getAnimationData(outlet: RouterOutlet) {
+    return outlet && outlet.activatedRouteData && outlet.activatedRouteData.animation;
    
     }
 
